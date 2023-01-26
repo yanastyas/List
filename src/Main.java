df656c2b65c596382d7992968d3e90d2f5c043e1
yanastyas <yanastyas28@mail.ru>
2023-01-27 02:24:13 +1000

    отдельные методы

--- src/Main.java
+++ src/Main.java
@@ -1,62 +1,80 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Scanner;
 
+import static jdk.nashorn.internal.objects.NativeString.toLowerCase;
+
 public class Main {
+    public static void showList(ArrayList<String> shoppingList) {
+        System.out.println("Список покупок: ");
+        for (int i = 0; i < shoppingList.size(); i++) {
+            System.out.println((i + 1) + " : " + shoppingList.get(i));
+        }
+    }
 
     public static void main(String[] args) {
+        ArrayList<String> shoppingList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
-        String[] operation = {"добавить", "показать", "удалить", "найти"};
-        System.out.println("Доступны операции: ");
-        for (int i = 0; i < operation.length; i++) {
-            System.out.println((i + 1) + ". " + operation[i]);
-        }
-        List<String> shoppingList = new ArrayList<>();
-
         while (true) {
-            System.out.println("Выберите операцию:");
+            System.out.println("Доступны операции: ");
+            System.out.println("\t 1 - Добавить. 2 - Показать. 3 - Удалить. 4 - Найти.");
             String input = scanner.nextLine();
+            System.out.println("Выберите операцию:");
 
-            if (input.equals("1")) {
-                System.out.println("Какую покупку хотите добавить? Введите название: ");
-                input = scanner.nextLine();
-                shoppingList.add(input);
-                System.out.println("Итого в списке покупок: " + shoppingList.size());
-            } else if (input.equals("2")) {
-                System.out.println("Список покупок: ");
-                for (int i = 0; i < shoppingList.size(); i++) {
-                    System.out.println((i + 1) + " : " + shoppingList.get(i));
-                }
-            } else if (input.equals("3")) {
-                try {
-                    System.out.println("Список покупок: ");
-                    for (int i = 0; i < shoppingList.size(); i++) {
-                        System.out.println((i + 1) + " : " + shoppingList.get(i));
-                    }
-                    System.out.println("Какую покупку хотите удалить? Введите номер или название: ");
-                    input = scanner.nextLine();
-                    int product = Integer.parseInt(input) - 1;
-                    System.out.println("Покупка " + shoppingList.get(product) + " - удалено, список продуктов: ");
-                    shoppingList.remove(product);
-                } catch (NumberFormatException s) {
-                    System.out.println("Покупка " + input + "- удалено, список покупок: ");
-                    shoppingList.remove(input);
-                }
-                for (int i = 0; i < shoppingList.size(); i++) {
-                    System.out.println((i + 1) + " :" + shoppingList.get(i));
-                }
-            } else if (input.equals("4")) {
-                System.out.println("Введите текст для поиска: ");
-                input = scanner.nextLine();
-                System.out.println("Найдено: ");
-                for (int i = 0; i < shoppingList.size(); i++) {
-                    String itemLower = shoppingList.get(i).toLowerCase();
-                    String queryLower = input.toLowerCase();
-                    if (itemLower.contains(queryLower)) {
-                        System.out.println((i + 1) + " : " + shoppingList.get(i));
-                    }
-                }
+            int operation = Integer.parseInt(input);
+            if (operation == 1) {
+                addProduct(shoppingList, scanner);
+            }
+            if (operation == 2) {
+                showList(shoppingList);
+            }
+            if (operation == 3) {
+                delProduct(shoppingList, scanner);
+            }
+            if (operation == 4) {
+                searchProduct(shoppingList, scanner);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void addProduct(ArrayList<String> shoppingList, Scanner scanner) {
+        System.out.println("Какую покупку хотите добавить? Введите номер или название: ");
+        String product = scanner.nextLine();
+        shoppingList.add(product);
+        System.out.println("Итого в списке покупок: " + shoppingList.size());
+    }
+
+    private static void delProduct(ArrayList<String> shoppingList, Scanner scanner) {
+        System.out.println("Какую покупку хотите удалить? Введите номер или название: ");
+        String input = scanner.nextLine();
+        int product = Integer.parseInt(input) - 1;
+        System.out.println("Покупка " + shoppingList.get(product) + " - удалено, список продуктов: ");
+        shoppingList.remove(product);
+        try {
+            System.out.println("Список покупок: ");
+            for (int i = 0; i < shoppingList.size(); i++) {
+                System.out.println((i + 1) + " : " + shoppingList.get(i));
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Покупка " + input + "- удалено, список покупок: ");
+            shoppingList.remove(input);
+        }
+    }
+
+    private static void searchProduct(ArrayList<String> shoppingList, Scanner scanner) {
+        System.out.println("Введите текст для поиска: ");
+        String input = scanner.nextLine();
+        String itemLower = toLowerCase(input);
+        System.out.println("\t Найдено: ");
+        for (int i = 0; i < shoppingList.size(); i++) {
+            String queryLower = toLowerCase(shoppingList.get(i));
+            if (queryLower.contains(itemLower)) {
+                System.out.println((i + 1) + " : " + shoppingList.get(i));
+
             }
         }
     }
 }
+
